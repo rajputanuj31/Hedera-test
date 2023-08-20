a0226129e2dba3171ccde5c3db802bd72bd455aa
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-08-20 23:48:44 +0530

    Basic random number

--- Backend/contracts/AIPrompt.sol
+++ Backend/contracts/AIPrompt.sol
@@ -1,94 +1,35 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.4.9 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./IPrngSystemContract.sol";
 
-contract AIPrompt is ERC721 {
+contract PrngSystemContract {
+    address constant PRECOMPILE_ADDRESS = address(0x169);
+    uint32 randNum;
 
-    constructor() ERC721("AI Prompt", "AIP") {}
-    
-    struct Raffle{
-        uint256 tokenId;
-        uint256 amount;
-        uint256 duration;
-        bool isOpen;
-        uint256 entries;
-        address [] addresses;
-    }
-    
-    mapping(address=>Raffle) private s_raffles;
-    mapping(uint256=>address) private s_raffleId;
-    mapping(address=>uint256) private s_proceeds;
-
-    uint256 raffleId = 0 ;
-    uint256 s_tokenId = 0 ;
-
-    function safeMint(address to) public {
-        _safeMint(to, s_tokenId);
-        s_tokenId = s_tokenId + 1;
+    function getPseudorandomSeed() external returns (bytes32 randomBytes) {
+        (bool success, bytes memory result) = PRECOMPILE_ADDRESS.call(
+            abi.encodeWithSelector(IPrngSystemContract.getPseudorandomSeed.selector));
+        require(success);
+        randomBytes = abi.decode(result, (bytes32));
     }
 
-    function createRaffle(uint256 _tokenId, uint256 duration, uint256 amount) public {
-        
-        // Make sure msg.sender has NFT
-        
-        Raffle memory newRaffle;
-        newRaffle.tokenId = _tokenId;
-        newRaffle.duration = duration;
-        newRaffle.amount = amount;
-
-        s_raffleId[raffleId] = msg.sender;
-        raffleId += 1;
-        s_raffles[msg.sender] = newRaffle;
-        
-    }
-
-    function enterRaffle(uint256 _raffleId) public payable{
-        address raffles_creator = s_raffleId[_raffleId];
-        Raffle storage raffleDetails = s_raffles[raffles_creator];
-
-        if(msg.value < raffleDetails.amount){
-            revert("Less than require amount sent");
+    /**
+     * Returns a pseudorandom number in the range [lo, hi) using the seed generated from "getPseudorandomSeed"
+     */
+    function getPseudorandomNumber(uint32 lo, uint32 hi) external returns (uint32) {
+        (bool success, bytes memory result) = PRECOMPILE_ADDRESS.call(
+            abi.encodeWithSelector(IPrngSystemContract.getPseudorandomSeed.selector));
+        require(success);
+        uint32 choice;
+        assembly {
+            choice := mload(add(result, 0x20))
         }
-
-        address [] storage temp = raffleDetails.addresses;
-        temp.push(msg.sender);
-        raffleDetails.addresses = temp;
-        
-        s_proceeds[raffles_creator] = s_proceeds[raffles_creator] + msg.value;
-        raffleDetails.entries = raffleDetails.entries + 1;
-
+        randNum = lo + (choice % (hi - lo));
+        return randNum;
     }
 
-    function getCurrentTokenId () public view returns (uint256){
-        // Current token ID of NFT
-        return s_tokenId;
+    function getNumber() public view returns (uint32) {
+        return randNum;
     }
-
-    function getParticipants (uint256 _raffleId) public view returns(address []memory){
-        // Should return array of participants
-        return s_raffles[ s_raffleId [ _raffleId ] ].addresses;
-    }
-
-    function particantCount (uint256 _raffleId) public view returns(uint256 ){
-        // Should return number of participants
-        return s_raffles[ s_raffleId [ _raffleId ] ].entries;
-    }
-
-    function raffleFee (uint256 _raffleId) public view returns(uint256 ){
-        // Should return price to enter 
-        return s_raffles[ s_raffleId [ _raffleId ] ].amount;
-    }
-    
-    function getRaffleTokenId (uint256 _raffleId) public view returns(uint256 ){
-        // Should return token Id
-        return s_raffles[ s_raffleId [ _raffleId ] ].tokenId;
-    }
-
-    function addressCreator(uint256 _raffleId) public view returns(address ){
-        // Should return who created raffle
-        return s_raffleId [ _raffleId ] ;
-    }
-
 }
--- Backend/index.js
+++ Backend/index.js
@@ -9,12 +9,13 @@ const {
     Hbar,
     FileCreateTransaction,
     TransferTransaction,
+    ContractExecuteTransaction,
+    ContractFunctionParameters,
+    TransactionRecordQuery,
+    ContractCallQuery
 } = require("@hashgraph/sdk");
 
-console.log(bytecode)
-
 require("dotenv").config();
-
 
 async function environmentSetup() {
 
@@ -35,21 +36,81 @@ async function environmentSetup() {
 
     client.setMaxQueryPayment(new Hbar(50));
 
-    const contractCreate = new ContractCreateFlow()
-        .setGas(500000)
-        .setBytecode(bytecode);
+    let gasLim = 4000000;
+    const [contractId, contractAddress] = await deployContractFcn(bytecode, gasLim, client);
+    console.log(`- Contract ID: ${contractId}`);
+    console.log(`- Contract ID in Solidity address format: ${contractAddress}`);
 
-    //Sign the transaction with the client operator key and submit to a Hedera network
-    const txResponse = contractCreate.execute(client);
-    console.log("TRANSACTION RES : ", txResponse);
+    // Execute the contract
+    const lo = 0;
+    const hi = 10;
+    const randNumParams = new ContractFunctionParameters().addUint32(lo).addUint32(hi);
+    const randNumRec = await executeContractFcn(contractId, "getPseudorandomNumber", randNumParams, gasLim, client);
+    console.log(`- Contract execution: ${randNumRec.receipt.status} \n`);
 
-    //Get the receipt of the transaction
-    const receipt = (await txResponse).getReceipt(client);
-    console.log("Receipt : ",receipt)
-    //Get the new contract ID
-    const newContractId = (await receipt).contractId;
+    const recQuery = await txRecQueryFcn(randNumRec.transactionId, client);
 
-    //Log the file ID
-    console.log("The smart contract byte code file ID is " + newContractId)
+    let lowOrderBytes = new Uint8Array(recQuery.children[0].prngBytes).slice(28, 32);
+    let dataview = new DataView(lowOrderBytes.buffer);
+    let range = hi - lo;
+    let int32be = dataview.getUint32(0);
+    let randNum = int32be % range;
+    console.log(`- The random number (using transaction record) = ${randNum}`)
+
+    const randNumResult = await callContractFcn(contractId, "getNumber", gasLim, client);
+    console.log(`- The random number (using contract function) = ${randNumResult.getUint32(0)}`);
+    randNum === randNumResult.getUint32(0) ? console.log(`- The random number checks out ✅`) : console.log(`- Random number doesn't match ❌`);
+
+    // Check a Mirror Node Explorer
+    const [randNumInfo, randNumExpUrl] = await mirrorTxQueryFcn(randNumRec.transactionId);
+    console.log(`\n- See details in mirror node explorer: \n${randNumExpUrl}`);
+
 }
 environmentSetup();
+
+async function deployContractFcn(bytecode, gasLim, client) {
+    const contractCreateTx = new ContractCreateFlow().setBytecode(bytecode).setGas(gasLim);
+    const contractCreateSubmit = await contractCreateTx.execute(client);
+    const contractCreateRx = await contractCreateSubmit.getReceipt(client);
+    const contractId = contractCreateRx.contractId;
+    const contractAddress = contractId.toSolidityAddress();
+    return [contractId, contractAddress];
+}
+async function executeContractFcn(cId, fcnName, params, gasLim, client) {
+    const contractExecuteTx = new ContractExecuteTransaction().setContractId(cId).setGas(gasLim).setFunction(fcnName, params);
+    const contractExecuteSubmit = await contractExecuteTx.execute(client);
+    const contractExecuteRec = await contractExecuteSubmit.getRecord(client);
+    return contractExecuteRec;
+}
+
+async function txRecQueryFcn(txId, client) {
+    const recQuery = await new TransactionRecordQuery().setTransactionId(txId).setIncludeChildren(true).execute(client);
+    return recQuery;
+}
+
+async function callContractFcn(cId, fcnName, gasLim, client) {
+    const contractCallTx = new ContractCallQuery().setContractId(cId).setGas(gasLim).setFunction(fcnName);
+    const contractCallSubmit = await contractCallTx.execute(client);
+    return contractCallSubmit;
+}
+
+async function mirrorTxQueryFcn(txIdRaw) {
+    // Query a mirror node for information about the transaction
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+    await delay(10000); // Wait for 10 seconds before querying a mirror node
+
+    const txIdPretty = prettify(txIdRaw.toString());
+    const mirrorNodeExplorerUrl = `https://hashscan.io/testnet/transaction/${txIdPretty}`;
+    const mirrorNodeRestApi = `https://testnet.mirrornode.hedera.com/api/v1/transactions/${txIdPretty}`;
+    let mQuery = [];
+    try {
+        mQuery = await axios.get(mirrorNodeRestApi);
+    } catch { }
+    return [mQuery, mirrorNodeExplorerUrl];
+}
+
+function prettify(txIdRaw) {
+    const a = txIdRaw.split("@");
+    const b = a[1].split(".");
+    return `${a[0]}-${b[0]}-${b[1]}`;
+}
