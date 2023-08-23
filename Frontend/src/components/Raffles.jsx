a73682056115afeeac67268e3f2b21ff7929f077
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-08-23 10:56:46 +0530

    Cards added

--- Frontend/src/components/Raffles.jsx
+++ Frontend/src/components/Raffles.jsx
@@ -1,16 +1,10 @@
 import { Client, Hbar, TopicMessageSubmitTransaction, TopicCreateTransaction } from '@hashgraph/sdk';
-
+import Cards from './Cards'
 import { client, url, topicId } from './helper'
 import { useEffect, useState } from 'react';
 import { Buffer } from 'buffer';
-
-// Material UI import
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-
+import sampleImage from './sample.png'; // Replace with the actual path to your image
+import hederaImage from './hedera.png';
 
 export default function () {
 
@@ -40,34 +34,33 @@ export default function () {
                 fill.push(formattedObject)
             }
             setRaffleDetails(fill);
+            console.log(fill)
         }
         getHedera()
     }, [])
 
     return (
         <div>
-                <Card sx={{ maxWidth: 345}} raised={true}>
-                    <CardActionArea>
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            image="/static/images/cards/contemplative-reptile.jpg"
-                            alt="green iguana"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                Lizard
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000
-                                species, ranging across all continents except Antarctica
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions>
+            <div className="card-container">
+                {raffleDetails.map((e, k) => (
+                    <div className="card">
+                        <img src={sampleImage} alt="Sample" className="card-image" />
+                        <div className='token_id'>
+                            Token Id: #{e.TokenId}
+                        </div>
+                        <div className='price-wrap'>
+                            <img src={hederaImage} className='hedera-image' />
+                            <h2 className="card-amount">{e.Amount} </h2>
+                        </div>
+                        <p className="card-owner">
+                            Created by: {(e.Creator).slice(0, 5)}...{(e.Creator).slice(-4)}
+                        </p>
+                        <button className="card-button">Enter Raffle</button>
+                    </div>
+                )
+                )}
+            </div>
 
-                    </CardActions>
-                </Card>
 
         </div>
     )
