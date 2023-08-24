a729f85745ad6c00b2250118703e00bac414f57a
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-08-25 01:03:26 +0530

    Sorry for mail :P

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
-	<MoralisProvider initializeOnMount={false}>
 
 	
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
-	</MoralisProvider>
 	,
 	
 	document.getElementById("root")
